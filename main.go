fdeb3e2b73c8f66a26f0c75cc8596ae771b4d05a
Ranjan Kumar Singh <137ranjan@gmail.com>
2025-04-02 20:47:50 +0530

    switch case and random number

--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/137ranjan/puppy"
+	"math/rand"
 )
 
 func main() {
-	s1 := puppy.Bark()
-	s2 := puppy.Barks()
-
-	fmt.Println(s1)
-	fmt.Println(s2)
-
-	s3 := puppy.BigBark()
-	s4 := puppy.BigBarks()
-
-	fmt.Println(s3)
-	fmt.Println(s4)
-
-	fmt.Println(puppy.Bark())
-	fmt.Println(puppy.Barks())
+	x := rand.Intn(10)
+	y := rand.Intn(10)
+	fmt.Printf("x = %v\n", x)
+	fmt.Printf("y = %v\n", y)
+	switch {
+	case x < 4 && y < 4:
+		fmt.Println("x and y are both less than 4")
+	case x > 6 && y > 6:
+		fmt.Println("x and y are both greater than 6")
+	case x >= 4 && x <= 6:
+		fmt.Println("x is greater than or equal to 4 and less than or equal to 6")
+	case y != 5:
+		fmt.Println("y is not equal to 5")
+	default:
+		fmt.Println("none")
+	}
 }
